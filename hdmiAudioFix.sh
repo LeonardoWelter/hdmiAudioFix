ac268d4bc962fdf9534627410c69462512b3c1f2
Leonardo Welter <50498606+LeonardoWelter@users.noreply.github.com>
2020-07-10 16:32:19 -0300

    v 1.1.0
     - Mudança nas mensagens de Feedback para facilitar o uso
     - Altera o dispositivo primário para o HDMI automaticamente

--- hdmiAudioFix.sh
+++ hdmiAudioFix.sh
@@ -4,23 +4,39 @@
 # através do HDMI utilizando o PulseAudio, quando ocorre um erro
 # no PulseAudio e os dispositivos não são detectados
 
-echo "+=======================================+"
-echo "|        HDMI Audio Fix Script          |"
-echo "|          by Leonado Welter            |"
-echo "+=======================================+"
+echo "+=================================================+"
+echo "|             HDMI Audio Fix Script               |"
+echo "|               by Leonado Welter                 |"
+echo "+=================================================+"
 
+echo -n "| Removendo arquivos antigos................"
+sleep 1
+if rm -R ~/.config/pulse/*; then # Remove os arquivos gerados pelo PulseAudio
+      echo "...OK |"
 
-if rm -R ~/.config/pulse/*; then
-      echo "|     Arquivos removidos com sucesso    |"
-    if pulseaudio -k; then
-      echo "| PulseAudio reconfigurado com sucesso! |"
+      echo -n "| Reconfigurando o PulseAudio.........."
+      sleep 1
+    if pulseaudio -k; then # Gera novamente a lista de dispositivos
+      echo -n "....."
+      sleep 1
+      echo "...OK |"
+
+      echo -n "| Alterando o dispositivo primário....."
+      sleep 1
+      echo -n "....."
+      sleep 1 # Necessário 2 segundos de espera para o término da geração da lista de dispositivos
+      if pacmd set-default-sink 0; then # Configura o dispositivo primário como sendo o HDMI
+        echo "...OK |"
+      else
+        echo "ERRO |"
+      fi # Fim IF configuração do dispositivo primário
     else
-      echo "| Não foi possível reconfigurar o pulseaudio. |"
+      echo "ERRO |" # Fim IF geração da lista de dispositivos
     fi
 else
-    echo "| Não foi possível remover os arquivos. |";
+    echo "ERRO |" # Fim IF Remoção de arquivos do PulseAudio
 fi
 
-echo "+=======================================+"
+echo "+=================================================+"
 sleep 2
 exit
